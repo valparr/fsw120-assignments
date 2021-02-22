679b2746189fb1e7fe54fe865b562ecea8f49ae6
valparr <valencia.parr@bryanuniversity.edu>
2021-02-21 19:58:04 -0600

    todo assignment

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+        <p>My Morning Routine</p>
+        <input type="checkbox" />Wake up<br/>
+        <input type="checkbox" />Brush Teeth<br/>
+        <input type="checkbox" />Make Coffee<br/>
+        </div>
+    )
+    
+}
+
+export default App
