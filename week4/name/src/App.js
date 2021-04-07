c2ad047a76c6b121ed9160902e189bcba7191e1d
valparr <valencia.parr@bryanuniversity.edu>
2021-04-07 10:33:55 -0500

    name entry assignment 1

--- week4/name/src/App.js
+++ week4/name/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import MyList from "./MyList"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super ()
+    this.state = {
+      name: "",
+      list: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleChange(event){
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+    event.preventDefault();
+  }
+
+  handleClick(event){
+    this.setState(prevState => ({
+        list: [...prevState.list, this.state.name]
+      })
+    )
+    event.preventDefault();
+    console.log(this.state.list)
+  }
+ 
+  render(){
+    const user = this.state.list.map(x => <MyList name={x}/>)
+    return (
+      <div>
+        <form>
+          <h1>NAME REGISTRY</h1>
+          <p>Please enter your name to be added to the Name Registry.</p>
+          <input id="inputField" type="text" name="name" placeholder="Enter Name" onChange={this.handleChange}/>
+          <button className="myButton" onClick={this.handleClick}>Submit Name</button>
+          <h1>{this.state.name}</h1>
+          {user}
+        </form>
+      </div>
+    )
+  } 
 }
 
-export default App;
+export default App
---
+++ week4/name/src/MyList.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function MyList(props) {
+  return (
+    <div>
+      <ul>
+        <li>{props.name}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyList
